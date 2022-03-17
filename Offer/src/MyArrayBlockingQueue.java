358a1654821e3bea46355890b4b5587eee825ca0
Joseph <lorypjh@163.com>
2022-03-17 16:22:52 +0800

    MyArrayBlockingQueue

---
+++ Offer/src/MyArrayBlockingQueue.java
@@ -0,0 +1,68 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyArrayBlockingQueue<E> {
+
+    private Object[] table;
+    private int capacity;
+    private int count;
+    private int putIndex;
+    private int takeIndex;
+
+    private ReentrantLock lock;
+    private Condition full;
+    private Condition empty;
+
+    public MyArrayBlockingQueue(int capacity) {
+        this.capacity = capacity;
+        this.table = new Object[capacity];
+        this.lock = new ReentrantLock();
+        this.full = lock.newCondition();
+        this.empty = lock.newCondition();
+    }
+
+    public void put(E e) throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == capacity){
+                full.await();
+            }
+            enqueue(e);
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public E get() throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == 0){
+                empty.await();
+            }
+            return dequeue();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    private void enqueue(E e) {
+        table[putIndex] = e;
+        if (++putIndex == capacity){
+            putIndex = 0;
+        }
+        count++;
+        empty.signalAll();
+    }
+
+    private E dequeue() {
+        E e = (E) table[takeIndex];
+        table[takeIndex] = null;
+        if (++takeIndex == capacity){
+            takeIndex = 0;
+        }
+        count--;
+        full.signalAll();
+        return e;
+    }
+
+}
