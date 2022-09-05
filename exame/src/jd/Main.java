84ba2b83c0fac004018be5aa563cc2d3f3ca5510
Joseph-Peng <lorypjh@163.com>
2022-09-05 20:28:16 +0800

    111

--- exame/src/jd/Main.java
+++ exame/src/jd/Main.java
@@ -1,7 +1,7 @@
 package jd;
 
 import java.math.BigInteger;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -9,21 +9,92 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        BigInteger x = sc.nextBigInteger();
-        sc.nextLine();
-        String path = sc.nextLine();
-        BigInteger one = new BigInteger("1");
-        BigInteger two = new BigInteger("2");
+        int[] nums = new int[n];
         for (int i = 0; i < n; ++i) {
-            if (path.charAt(i) == 'U') {
-                x = x.divide(two);
-            } else if (path.charAt(i) == 'R') {
-                x = x.multiply(two);
-                x = x.add(one);
-            } else {
-                x = x.multiply(two);
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        double[] res = new double[nums.length - k + 1];
+        int[] window = new int[k];
+        //添加初始值
+        for (int i = 0; i < k; i++) {
+            window[i] = nums[i];
+        }
+
+        Arrays.sort(window);
+        double mid = getMid(window);
+        double a = mid - window[0];
+        double b = window[k-1] - mid;
+        if(a > b){
+            res[0] = window[0];
+        }else {
+            res[0] = window[k-1];
+        }
+        for (int i = 0; i < nums.length - k; i++) {
+            int index = search(window, nums[i]);
+            window[index] = nums[i + k];
+            while (index < window.length - 1 && window[index] > window[index + 1]) {
+                swap(window, index, index + 1);
+                index++;
+            }
+            while (index > 0 && window[index] < window[index - 1]) {
+                swap(window, index, index - 1);
+                index--;
+            }
+            mid = getMid(window);
+            a = mid - window[0];
+            b = window[k-1] - mid;
+            if(a > b){
+                res[i + 1] = window[0];
+            }else {
+                res[i + 1] = window[k-1];
             }
         }
-        System.out.println(x);
+        StringBuffer ans = new StringBuffer();
+        ans.append("[");
+
+        for (int i = 0; i < res.length; ++i){
+            ans.append((int) res[i] + ", ");
+        }
+        ans.deleteCharAt(ans.length()-1);
+        ans.deleteCharAt(ans.length()-1);
+        ans.append("]");
+        System.out.println(ans.toString());
+    }
+
+    //求数组的中位数
+    private static double getMid(int[] window) {
+        int len = window.length;
+        if (window.length % 2 == 0) {
+            //避免溢出
+            return window[len / 2] / 2.0 + window[len / 2 - 1] / 2.0;
+        } else {
+            return window[len / 2];
+        }
+    }
+
+    //交换
+    private static void swap(int[] window, int i, int j) {
+        int temp = window[i];
+        window[i] = window[j];
+        window[j] = temp;
+    }
+
+    //最简单的二分查找
+    private static int search(int[] window, int target) {
+        int start = 0;
+        int end = window.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (window[mid] > target) {
+                end = mid - 1;
+            } else if (window[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
+// [2452, 2452, 2452, 1284]
--- exame/src/shence/No3.java
+++ exame/src/shence/No3.java
@@ -1,5 +1,10 @@
 package shence;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * @author Joseph Peng
  * @date 2022/8/13 9:54
@@ -7,6 +12,20 @@ package shence;
 public class No3 {
 
     public static void main(String[] args) {
-
+        String str = "faxxxxfailed";
+        int start = str.length() - "failed".length();
+        int end = str.length();
+        System.out.println(str.substring(start, end));
+        TreeMap<Character, Integer> map = new TreeMap<>(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return 0;
+            }
+        });
+        map.put('a',0);
+        map.put('c',1);
+        map.put('b',0);
+        map.put('d',2);
+        System.out.println(map.firstKey());
     }
 }
